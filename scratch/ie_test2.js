abce0d7b9e2f9a4950287d0d736a538d8441bedb
Joel Meyer <joel.meyer@gmail.com>
2012-04-26 10:25:41 -0700

    add divs

--- scratch/ie_test2.js
+++ scratch/ie_test2.js
@@ -1,5 +1,5 @@
 (function() {
   var script_tags = document.getElementsByTagName("script");
   var this_script = script_tags[script_tags.length-1];
-  document.write("This script tag has src: " + this_script.src);
+  document.write("script2: This script tag has src: " + this_script.src);
 })();
--- scratch/ie_test3.js
+++ scratch/ie_test3.js
@@ -1,5 +1,5 @@
 (function() {
   var script_tags = document.getElementsByTagName("script");
   var this_script = script_tags[script_tags.length-1];
-  document.write("This script tag has src: " + this_script.src);
+  document.write("script3: This script tag has src: " + this_script.src);
 })();
